01e49d18d0fb825bb7e4ad058d456bc23e2b781b
+ <+>
2024-03-16 10:20:26 -0700

    complete lab

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,24 @@
-import React from "react";
+import React, {useState} from "react";
 import SushiContainer from "./SushiContainer";
 import Table from "./Table";
 
-const API = "http://localhost:3001/sushis";
 
 function App() {
+  const [plates, setPlates] = useState([])
+  const [dollars, setDollars] = useState(100)
+
+  function addPlate(){
+    setPlates([...plates, ''])
+  }
+
+  function buySushi(price){
+    setDollars(dollars - price)
+  }
+
   return (
     <div className="app">
-      <SushiContainer />
-      <Table />
+      <SushiContainer addPlate={addPlate} buySushi={buySushi} dollars={dollars}/>
+      <Table plates={plates} dollars={dollars}/>
     </div>
   );
 }
--- src/components/SushiContainer.js
+++ src/components/SushiContainer.js
@@ -1,11 +1,37 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 import MoreButton from "./MoreButton";
+import Sushi from "./Sushi";
 
-function SushiContainer(props) {
+const API = "http://localhost:3001/sushis";
+
+function SushiContainer({addPlate, buySushi, dollars}) {
+  const [sushiData, setSushiData] = useState([])
+  const [slicePoint, setSlicePoint] = useState(0)
+
+  useEffect(() => fetch("http://localhost:3001/sushis")
+  .then(response => response.json())
+  .then(data => {
+    setSushiData(data.slice(slicePoint, slicePoint+4))
+  }), [slicePoint])
+
+  function moreSushi(){
+    setSlicePoint(slicePoint + 4)
+  }
+
   return (
     <div className="belt">
-      {/* Render Sushi components here! */}
-      <MoreButton />
+      {sushiData.map(sushi => 
+      <Sushi 
+        key={sushi.id} 
+        id={sushi.id} 
+        name={sushi.name}
+        img_url={sushi.img_url}
+        price={sushi.price}
+        addPlate={addPlate}
+        buySushi={buySushi}
+        dollars={dollars}
+      />)}
+      <MoreButton moreSushi={moreSushi}/>
     </div>
   );
 }
